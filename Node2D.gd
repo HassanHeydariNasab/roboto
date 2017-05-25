3205b7b87c38edaca4edb0773c7768a0a33ac15b
hsn6 <hsn6@openmailbox.org>
2017-05-25 11:27:00 +0430

    antaux kreema kulgo

--- Node2D.gd
+++ Node2D.gd
@@ -8,6 +8,7 @@ const ZOOM_SPEED = 1
 var atendado_z = 0
 var muso_lasita = false
 var muso_lasado_konsumita = true
+var muso_tenado = 0
 var os = OS.get_name()
 
 onready var K = get_node("K/RigidBody2D")
@@ -26,26 +27,38 @@ func _input(evento):
 		if evento.is_pressed() == 1:
 			muso_lasita = false
 			muso_lasado_konsumita = false
+			muso_tenado += 1
 		elif evento.is_pressed() == 0:
 			muso_lasita = true
 			#Vi devas konsumi la laseco de la muso
 		
 func _process(delta):
 	var akcelometro = Input.get_accelerometer()
-	if Input.is_action_pressed("iri") or (akcelometro.x < -3 and os == "Android"):
+	if (Input.is_action_pressed("iri") and Input.is_action_pressed("malrapidi")) or (os == "Android" and akcelometro.x > -3 and akcelometro.x < -1):
+		K.set_angular_velocity(2)
+	elif (Input.is_action_pressed("reveni") and Input.is_action_pressed("malrapidi")) or (os == "Android" and akcelometro.x < 3 and akcelometro.x > 1):
+		K.set_angular_velocity(-2)
+	elif Input.is_action_pressed("iri") or (os == "Android" and akcelometro.x < -3):
 		K.set_angular_velocity(6)
-	elif Input.is_action_pressed("reveni") or (akcelometro.x > 3 and os == "Android"):
+	elif Input.is_action_pressed("reveni") or (os == "Android" and akcelometro.x > 3):
 		K.set_angular_velocity(-6)
-	if (Input.is_action_pressed("salti") or (akcelometro.y > -1 and os == "Android")) and T in K.get_colliding_bodies():
-		print(akcelometro)
+	if (Input.is_action_pressed("salti") or (os == "Android" and akcelometro.y > -1)) and T in K.get_colliding_bodies():
 		K.set_linear_velocity(Vector2(K.get_linear_velocity().x, -200))
 	if not pafita:
+		#Kuglo.set_scale(Vector2(1, 1))
+		#Kuglo_s.set_scale(Vector2(1, 1))
 		var a = Kuglo.get_angle_to(get_global_mouse_pos())
 		Kuglo_s.set_rot(a)
 		Kuglo.set_global_pos(Vector2(K.get_global_pos().x+10, K.get_global_pos().y-110))
+	if Input.is_action_pressed("pafi"):
+		muso_tenado += 1
 	if muso_lasita and not muso_lasado_konsumita and atendado_p == 0 and not pafita:
 		muso_lasado_konsumita = true
 		pafita = true
+		Kuglo.set_scale(Vector2(muso_tenado, muso_tenado))
+		Kuglo_s.set_scale(Vector2(muso_tenado/100+1, muso_tenado/100+1))
+		print(muso_tenado)
+		muso_tenado = 0
 		var a = Kuglo.get_angle_to(get_global_mouse_pos())
 		Kuglo_s.set_rot(a)
 		Kuglo.set_linear_velocity(Vector2(cos(a)*3000, -sin(a)*3000))
